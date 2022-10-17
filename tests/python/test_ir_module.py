5bfd786329cb0cd083a0159acf827ef45c578365
SYangDong <shouyang.dong@gmail.com>
2022-10-17 14:54:42 +0800

    update test function

--- tests/python/test_ir_module.py
+++ tests/python/test_ir_module.py
@@ -3,22 +3,21 @@ from .transform import tir as T
 
 
 
-def mma_desc(a: T.handle, b: T.handle, c: T.handle) -> None:
-    A = T.match_buffer(a, (16, 16), align=64, offset_factor=1)
-    B = T.match_buffer(b, (16, 16), align=64, offset_factor=1)
-    C = T.match_buffer(c, (16, 16), align=64, offset_factor=1)
-
-    with T.block("root"):
-        T.reads(C[0 : 16, 0 : 16], A[0 : 16, 0 : 16], B[0 : 16, 0 : 16])
-        T.writes(C[0 : 16, 0 : 16])
-        for i, j, k in T.grid(16, 16, 16):
-            with T.block("update"):
-                vii, vjj, vkk = T.axis.remap("SSR", [i, j, k])
-                C[vii, vjj] = C[vii, vjj] + A[vii, vkk] * B[vjj, vkk]
+def matmul(a: T.handle, b: T.handle, c: T.handle) -> None:  # pylint: disable=no-self-argument
+    T.func_attr({"global_symbol": "matmul", "tir.noalias": True})
+    A = T.match_buffer(a, (1024, 1024), "float32")
+    B = T.match_buffer(b, (1024, 1024), "float32")
+    C = T.match_buffer(c, (1024, 1024), "float32")
+    for i, j, k in T.grid(1024, 1024, 1024):
+        with T.block("matmul"):
+            vi, vj, vk = T.axis.remap("SSR", [i, j, k])
+            with T.init():
+                C[vi, vj] = 0.0
+            C[vi, vj] = C[vi, vj] + A[vi, vk] * B[vk, vj]
 
 
 def test_func():
-    func = t.prim_func(mma_desc)
+    func = t.prim_func(matmul)
 
 if __name__ == "__main__":
     test_func()
